ed508c125d1d18208a510ee7cf95c1a02b01dedf
Huy Tran <huy.hoang2225@gmail.com>
2019-09-13 10:02:07 +0700

    update new verrsion

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-import CheckBox from './src/CheckBox';
-import RippleButton from './src/RippleButton';
-import RippleIcon from './src/RippleIcon';
-import RippleTouchable from './src/RippleTouchable';
-export {
-    CheckBox,
-    RippleButton,
-  	RippleIcon,
-  	RippleTouchable
-};
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
